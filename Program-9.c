d0883b30c369e153c55cf74453f0ee4f92d077c8
ruaimmohammed <20me0115@vvce.ac.in>
2021-03-02 17:19:05 +0000

    Program 9

--- Program-9.c
+++ Program-9.c
@@ -8,27 +8,27 @@ int main()
 {
   int array[100], search, c, n;
 
-  printf("Enter number of elements in the array, array elements and the key");
+  printf("Enter number of elements in the array, array elements and the key: ");
   scanf("%d", &n);
 
-  
+  printf("Enter the array elements: ");
   for (c = 0; c < n; c++)
+  {
     scanf("%d", &array[c]);
-
- 
+  }
+  printf("Enter the search element: ");
   scanf("%d", &search);
 
   for (c = 0; c < n; c++)
   {
     if (array[c] == search)    /* If required element is found */
     {
-      //printf("%d is present at location %d", search, c+1);
-      printf("%d",c+1);
+      printf("%d is present at location %d", search, c+1);
       break;
     }
   }
   if (c == n)
-    printf("%d is not present in the array", );
+    printf("%d is not present in the array",c);
 
   return 0;
 }
