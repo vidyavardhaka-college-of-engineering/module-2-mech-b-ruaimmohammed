f76a7a15b7a212444a994a84fffe5ee6193f5b74
ruaimmohammed <20me0115@vvce.ac.in>
2021-03-02 16:56:54 +0000

    Program 6

--- Program-6.c
+++ Program-6.c
@@ -2,7 +2,7 @@
 //OUTPUT- Total number of even numbers and odd numbers in the array are 1 and 2
 
 //INSERT THE MISSING CODE 
-
+#include<stdio.h>
 int main()
 {
     int arr[10];
@@ -13,7 +13,7 @@ int main()
     scanf("%d",&num);
     //printf("Enter %d elements in array: ", num);
         //printf("Enter the elements");
-    for(i=0;i<n;i++)
+    for(i=0;i<num;i++)
     {
         scanf("%d",&arr[i]);//missing code
     }
@@ -21,12 +21,12 @@ int main()
     evennum = 0; // Assuming 0 even numbers
     oddnum  = 0; // Assuming 0 odd numbers
 
-    for(i=0; i<n; i++)
+    for(i=0; i<num; i++)
     {
         /* If the current element of array is evennumber then increment evennumber count */
         if(arr[i]%2 == 0) //missing code
         {
-            ++;
+            evennum++;
         }
         else
         {
